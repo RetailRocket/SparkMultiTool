d64ea25bd75d52b98592dc502cad85fca94141ce
Artem Noskov <tik@n3.server.retailrocket.ru>
2014-10-08 12:48:37 +0400

    update example project

--- example/src/main/scala/MyProject.scala
+++ example/src/main/scala/MyProject.scala
@@ -2,12 +2,12 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-import ru.retailrocket.spark.multitool.Loaders
+import ru.retailrocket.spark.multitool.Loaders._
 
 object MyProject {
   def main(args: Array[String]) {
     val sc = new SparkContext("local", "MyProject")
-    val sessions = Loaders.combineTextFile(sc, "file://" + getClass.getResource("src/main/resource").getFile)
+    val sessions = sc.combineTextFile("file://" + getClass.getResource("src/main/resource").getFile)
     println(sessions.count)
   }
 }
