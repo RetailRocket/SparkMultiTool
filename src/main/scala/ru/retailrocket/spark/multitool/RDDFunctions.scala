9d191ed278bf684f2290c230f7e4b9e380f2cc88
Artem Noskov <tik@n3.server.retailrocket.ru>
2015-02-26 16:17:54 +0300

    add rdd saveAsMultipleTextFiles functions

--- src/main/scala/ru/retailrocket/spark/multitool/RDDFunctions.scala
+++ src/main/scala/ru/retailrocket/spark/multitool/RDDFunctions.scala
@@ -8,10 +8,14 @@ import org.apache.spark.rdd._
 import org.apache.hadoop.mapreduce.RecordReader
 import org.apache.hadoop.mapreduce.TaskAttemptContext
 import org.apache.hadoop.mapreduce.InputSplit
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.mapred.FileOutputFormat
 import org.apache.hadoop.io.compress.CompressionCodecFactory
 import org.apache.hadoop.util.LineReader
 import org.apache.hadoop.io._
 import org.apache.hadoop.fs._
+import org.apache.hadoop.io.compress._
 import org.apache.hadoop.mapreduce.lib.input._
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.conf.Configured
@@ -47,5 +51,39 @@ object RDDFunctions {
     def transform[R:ClassTag](f: T=>R)(implicit d: DummyImplicit): TransformResult[T,R] = {
       RDDFunctions.transform(f)(self)
     }
+  }
+
+  class KeyBasedMultipleTextOutputFormat extends MultipleTextOutputFormat[Text, Text] {
+    override def generateFileNameForKeyValue(key: Text, value: Text, name: String): String = {
+      key.toString + "/" + name
+    }
+    
+    override def generateActualKey(key: Text, value: Text) = null
+  }
+
+  def saveAsMultipleTextFiles[T:ClassTag](src: RDD[T], extractPath: T => String, root: String) {
+    saveAsMultipleTextFiles(src, extractPath, root, None)
+  }
+
+  def saveAsMultipleTextFiles[T:ClassTag](src: RDD[T], extractPath: T => String, root: String, codec: Class[_ <: CompressionCodec]) {
+    saveAsMultipleTextFiles(src, extractPath, root, Option(codec))
+  }
+
+  def saveAsMultipleTextFiles[T:ClassTag](src: RDD[T], extractPath: T => String, root: String, codec: Option[Class[_ <: CompressionCodec]]) {
+    val hadoopConf = new Configuration()
+    val jobConf = new JobConf(hadoopConf)
+
+    jobConf.setOutputFormat(classOf[KeyBasedMultipleTextOutputFormat])
+
+    if(codec.isDefined) {
+      jobConf.setBoolean("mapred.output.compress", true)
+      jobConf.setClass("mapred.output.compression.codec", codec.get, classOf[CompressionCodec])
+    }
+
+    FileOutputFormat.setOutputPath(jobConf, new Path(root))
+
+    src
+      .map{v => (new Text(extractPath(v)), new Text(v.toString))}
+      .saveAsHadoopDataset(jobConf)
   }
 }
