5a9bdc37c731c3f62cd98be0c8ffb247c00c5cb8
Artem Noskov <tik@n1.server.retailrocket.ru>
2016-02-16 15:19:17 +0300

    add fs rename funciton

--- src/main/scala/ru/retailrocket/spark/multitool/fs/package.scala
+++ src/main/scala/ru/retailrocket/spark/multitool/fs/package.scala
@@ -73,6 +73,14 @@ package object fs {
     fs.rename(tmpPath, distPath)
   }
 
+  def rename(src: String, dst: String) = {
+    val srcPath = new Path(src)
+    val dstPath = new Path(dst)
+
+    val fs = FileSystem.get(new Configuration())
+    fs.rename(srcPath, dstPath)
+  }
+
   def replace(src: String, dst: String) = {
     val srcPath = new Path(src)
     val dstPath = new Path(dst)
